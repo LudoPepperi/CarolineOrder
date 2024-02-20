6bedc90cba96ea9302f56845c83e283cf2ca95e7
LudoPepperi <77826528+LudoPepperi@users.noreply.github.com>
2024-02-20 17:38:51 +0100

    Update config_headerEN.js

--- config_headerEN.js
+++ config_headerEN.js
@@ -48,12 +48,12 @@ const LeftMenu = [
     action:"createNewActivity",
     deepLink: 'activities/details/{{UUID}}'
   },
-	{
+	/*{
     activity:"SAV New",
     title: "SAV",
     action:"createNewActivity",
     deepLink: 'activities/details/{{UUID}}'
-  },
+  },*/
  
 ];
 const RightMenu = [
